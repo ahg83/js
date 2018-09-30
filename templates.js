c443cab23293e6eb89d7ae4a56dd109e4bb4266c
Alberto Hidalgo <albhga@gmail.com>
2018-09-30 18:31:47 +0200

    delete function dos() y function tres()

--- templates.js
+++ templates.js
@@ -5,11 +5,3 @@ function countElements() {
     var lcountElements
 	document.getElementById("total").innerHTML = "=" + document.getElementsByTagName('p').length;
 }
-
-function dos(){
-
-}
-
-function tres(){
-  
-}
