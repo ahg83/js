3a2ee3c46d14cb9ce89494c7aa1d227f1e475869
Alberto Hidalgo <albhga@gmail.com>
2018-09-30 22:51:22 +0200

    function1: getElementsByTagName alert con el contenido de tags p

--- templates.js
+++ templates.js
@@ -5,3 +5,11 @@ function countElements() {
     var lcountElements
 	document.getElementById("total").innerHTML = "=" + document.getElementsByTagName('p').length;
 }
+function function1() {
+    var lsItem = document.getElementsByTagName("p");
+    var i;
+
+    for (i = 0; i < lsItem.length; i++){
+		alert(lsItem[i].innerHTML);
+    };
+}
